405c6da857db75629c5e57b25e4525043e0520fb
cannedbeef <cannedbeef255@proton.me>
2026-03-20 09:33:37 +1100

    implemented extension

--- taskE.py
+++ taskE.py
@@ -15,5 +15,15 @@ new_name  = input('Enter student name: ')
 new_score = input('Enter their score: ')
 results.append([new_name, new_score])
 
+# ROBIN!!! i see that em-dash!!!! (YOU SNEAKY BASTARD!!!!)
+#
+#                 |
+#                 v
 # Your code below — write the updated list back to scores.csv
 # Each line should be:  name,score
+
+def write_csv(filename, data):
+	with open(filename, "w") as file:
+		file.writelines([",".join(row)+"\n" for row in data])
+
+write_csv("scores.csv", results)
