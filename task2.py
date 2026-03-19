bdb2cdefe27908d2542acbd7c72aa32c1fc6a758
cannedbeef <cannedbeef255@proton.me>
2026-03-20 09:17:42 +1100

    implemented task 2

--- task2.py
+++ task2.py
@@ -12,3 +12,21 @@ file.close()
 city_to_find = input('Enter a city: ')
 
 # Your code below — find all students from that city
+
+ST_NAME: int = 0
+ST_AGE: int = 1
+ST_CITY: int = 2
+
+filtered_students = list(filter(
+	lambda student: student[ST_CITY].casefold() == city_to_find.casefold(),
+	students
+))
+
+if len(filtered_students) == 0:
+	print(f"No students live in \"{city_to_find}\"")
+
+else:
+	print(f"All students who live in \"{city_to_find}\":")
+
+	for student in filtered_students:
+		print(f"\t{student[ST_NAME]}: {student[ST_AGE]}")
